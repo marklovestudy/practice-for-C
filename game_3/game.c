157d3a8a3573660652776a7e41b2d37da6999aa3
marklovestudy <129322870+marklovestudy@users.noreply.github.com>
2023-04-11 01:47:45 +0800

    Add files via upload

---
+++ game_3/game.c
@@ -0,0 +1,146 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include "game.h"
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		//1打印一行数据
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+		//2打印分割行
+		if (i<row-1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+					printf("|");
+			}
+			printf("\n");
+		}
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家走：\n");
+	while (1)
+	{
+		printf("请输入要下的坐标：>");
+		scanf("%d%d", &x, &y);
+		//判断x,y坐标的合法性
+		if (x >= 1 && x <= 3 && y >= 1 && y <= 3)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("该坐标被点用\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，重新输入：\n");
+		}
+	}
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("电脑走：\n");
+	while (1)
+	{
+		x = rand() % 3;
+		y = rand() % 3;
+		if (board[x][y]==' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+
+int IsFull(char board[ROW][COL],int row,int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		//三行相同
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+		{
+			return board[i][0];
+		}
+	}
+	for (j = 0; j < col; j++)
+	{
+		//三列相同
+		if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ')
+		{
+			return board[0][j];
+		}
+	}
+	//二条对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[0][0];
+	}
+	if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ')
+	{
+		return board[0][0];
+	}
+	//判断是否平局
+	if (1 == IsFull(board, ROW, COL))
+	{
+		return 'Q';
+	}
+	return 'C';
+}
---
+++ 扫雷/text.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include "game.h"
+
+void game()
+{
+	//布置好雷的信息
+	char mine[ROWS][COLS] = { 0 };
+	//排查出雷的信息
+	char show[ROWS][COLS] = { 0 };
+	//初始化
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	//打印棋盘
+	DisplayBoard(mine, ROW, COL);
+	DisplayBoard(show, ROW, COL);
+	//开始布雷
+	SetMine(mine, ROW, COL);
+	DisplayBoard(mine, ROW, COL);
+	//扫雷
+	FineMine(mine,show, ROW, COL);
+}
+
+void menu()
+{
+	printf("********************************\n");
+	printf("****1 play 0 exit***************\n");
+	printf("********************************\n");
+}
+
+void test()
+{
+	//询问游戏是否开始，1开始，0退出
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			//开始游戏
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，重新输入：\n");
+			break;
+		}
+	} while (input);
+}
+
+int main()
+{
+	test();
+	return 0;
+}
